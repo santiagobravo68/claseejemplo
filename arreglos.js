f3a87899acc0f613a8b319d606388cea51a6f665
santiagobravo68 <bravocampok@gmail.com>
2026-09-11 11:06:13 -0500

    quinto commit

---
+++ arreglos.js
@@ -0,0 +1,14 @@
+let lista= []
+    lista.push("a")
+    console.log(lista)
+    lista.push("b")
+    console.log(lista)
+//Agregar al inicio de un arreglo unshif
+lista.unshift("c")
+console.log(lista)
+//pop ->eliminia elementos al final
+lista.pop()
+console.log(lista)
+//shift -> elimina al inicio
+lista.shift()
+console.log(lista)
--- logica.js
+++ logica.js
@@ -1,2 +1,71 @@
-console.log("hola mundo")
-console.log("Soy Santiago")
+class Animal {
+    nombre;
+    raza;
+    especie;
+    edad;
+    
+    constructor(nombre, raza, especie, edad) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.especie = especie;
+        this.edad = edad;
+    }
+    
+    registrarNombre(nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+    registrarRaza(nuevaRaza) {
+        this.raza = nuevaRaza;
+    }
+    registrarEspecie(nuevaEspecie) {
+        this.especie = nuevaEspecie;
+    }
+    registrarEdad(nuevaEdad) {
+        this.edad = nuevaEdad;
+    }
+    
+    // Corregido el nombre del método y añadido espacio
+    mostrarInformacion() {
+        console.log(this.nombre + " " + this.raza);
+    }
+}
+
+class Propietario {
+    nombrePropietario;
+    Direccion;
+    Telefono;
+    
+    constructor(nombrePropietario, Direccion, Telefono) {
+        this.nombrePropietario = nombrePropietario;
+        this.Direccion = Direccion;
+        this.Telefono = Telefono;
+        this.mascota = [];
+    }
+    
+    registrarNombre(nuevoNombre) {
+        this.nombrePropietario = nuevoNombre;
+    }
+    registrarDireccion(nuevaDireccion) {
+        this.Direccion = nuevaDireccion;
+    }
+    registrarTelefono(nuevoTelefono) {
+        this.Telefono = nuevoTelefono;
+    }
+    registrarMascota(nuevaMascota) {
+        this.mascota.push(nuevaMascota);
+    }
+    
+    mostrarInformacion() {
+        // Corregido para acceder al .nombre de la mascota y dar formato
+        console.log("Mascota: " + this.mascota[0].nombre + " | Propietario: " + this.nombrePropietario);
+    }
+}
+
+let colmillo = new Animal("Colmillo", "Pitbull", "perro", 12);
+colmillo.mostrarInformacion(); 
+// Imprime: Colmillo Pitbull
+
+let alexa = new Propietario("Alexa", "123", "1111");
+alexa.registrarMascota(colmillo);
+alexa.mostrarInformacion(); 
+// Imprime: Mascota: Colmillo | Propietario: Alexa
